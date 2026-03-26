988afb50535cfae37831d577fc111bb920a91fb8
Adrián Maza <adri_mv1@hotmail.com>
2026-03-26 12:23:59 +0100

    form component added

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
 export class TaskForm {
+  private taskService = inject(TaskService);
 
+  title = '';
+
+  addTask(): void {
+    const cleanTitle = this.title.trim();
+
+    if(!cleanTitle) return;
+
+    this.taskService.addTask(cleanTitle);
+    this.title = '';
+  }
 }
