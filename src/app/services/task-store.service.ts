ab9b5c97b3ecce62625ede6e62e03f86ee3261d8
Adrián Maza <adri_mv1@hotmail.com>
2026-03-26 19:23:50 +0100

    service created again

---
+++ src/app/services/task-store.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, signal } from '@angular/core';
+import { Task } from '../Models/task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskStoreService {
+  private nextId = 3;
+
+  tasks = signal<Task[]>([
+    {
+      id: 1,
+      title: 'Crear proyecto',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Añadir componentes y servicios',
+      completed: false,
+    },
+  ]);
+
+  addTask(title: string): void {
+    const newTask: Task = {
+      id: this.nextId++,
+      title,
+      completed: false,
+    };
+
+    this.tasks.update((currentTasks) => [...currentTasks, newTask]);
+  }
+
+  toggleTask(id: number): void {
+    this.tasks.update((currentTasks) =>
+      currentTasks.map((task) => (task.id === id ? { ...task, completed: !task.completed } : task)),
+    );
+  }
+}
