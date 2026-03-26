742fbd185364b4959a2138c68ae84efd80894db1
Adrián Maza <adri_mv1@hotmail.com>
2026-03-26 19:54:03 +0100

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./pages/tasks-page/tasks-page').then((m) => m.TasksPage),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
