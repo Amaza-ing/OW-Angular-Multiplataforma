f63e39d17a7579083665adbfc170724787df45bc
Adrián Maza <adri_mv1@hotmail.com>
2026-03-26 14:00:52 +0100

    task-list completed

--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,12 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { TaskService } from '../../services/task.service';
+import { Component, input, output } from '@angular/core';
+import { Task } from '../../Models/task.model';
+import { TaskItem } from "../task-item/task-item";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskItem],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
 export class TaskList {
-  taskService = inject(TaskService);
+  tasks = input.required<Task[]>();
+
+  taskToggled = output<number>();
 }
