50a880ebaaf056f66da5092180b10bfb007df9cb
Adrián Maza <adri_mv1@hotmail.com>
2026-04-03 18:33:46 +0200

    tasks page & about

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [IonContent],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
--- src/app/pages/tasks-page/tasks-page.ts
+++ src/app/pages/tasks-page/tasks-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskForm } from "../../components/task-form/task-form";
 import { TaskList } from "../../components/task-list/task-list";
+import { IonHeader, IonToolbar, IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-tasks-page',
-  imports: [TaskForm, TaskList],
+  imports: [TaskForm, TaskList, IonHeader, IonToolbar, IonContent],
   templateUrl: './tasks-page.html',
   styleUrl: './tasks-page.css',
 })
