46e313df87afe06282ebf00688c0aeb26683c851
Adrián Maza <adri_mv1@hotmail.com>
2026-04-03 17:00:21 +0200

    ionic capacitor

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+// npm install @ionic/angular @capacitor/core @capacitor/cli @capacitor/android
+// npm install ionicons
+// npx cap init angular-multiplataforma com.amaza.angularmultiplataforma
+// npx cap add android
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
