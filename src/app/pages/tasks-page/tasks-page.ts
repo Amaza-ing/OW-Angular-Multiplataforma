65c6e109b6c2894137016aefd13dc8694979cd69
Adrián Maza <adri_mv1@hotmail.com>
2026-04-04 11:08:33 +0200

    base completed

--- src/app/pages/tasks-page/tasks-page.ts
+++ src/app/pages/tasks-page/tasks-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { MobileHeader } from "../../components/mobile-header/mobile-header";
+import { TaskForm } from "../../components/task-form/task-form";
+import { TaskList } from "../../components/task-list/task-list";
 
 @Component({
   selector: 'app-tasks-page',
-  imports: [IonContent, MobileHeader],
+  imports: [IonContent, MobileHeader, TaskForm, TaskList],
   templateUrl: './tasks-page.html',
   styleUrl: './tasks-page.css',
 })
