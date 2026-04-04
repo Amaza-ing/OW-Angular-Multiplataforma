cfafc2609d4b8e8f294a6f9efe7de8e0a216fc8a
Adrián Maza <adri_mv1@hotmail.com>
2026-04-04 11:26:32 +0200

    home completed

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonContent, IonButton } from '@ionic/angular/standalone';
-import { MobileHeader } from "../../components/mobile-header/mobile-header";
+import {
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+} from '@ionic/angular/standalone';
+import { MobileHeader } from '../../components/mobile-header/mobile-header';
 
 @Component({
   selector: 'app-home',
-  imports: [IonContent, IonButton, RouterLink, MobileHeader],
+  imports: [
+    IonContent,
+    IonButton,
+    RouterLink,
+    MobileHeader,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
