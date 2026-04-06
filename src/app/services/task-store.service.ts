baaa7da1ed17e9de979d7cc80988bbcc3d1395f4
Adrián Maza <adri_mv1@hotmail.com>
2026-04-06 12:12:07 +0200

    localstorage added

--- src/app/services/task-store.service.ts
+++ src/app/services/task-store.service.ts
@@ -1,30 +1,42 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, effect, Injectable, signal } from '@angular/core';
 import { Task } from '../Models/task.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskStoreService {
-  private nextId = 3;
+  private storageKey = 'tasks';
+  private storedTasks = localStorage.getItem(this.storageKey);
+  private initialTasks: Task[] = this.storedTasks
+    ? JSON.parse(this.storedTasks)
+    : [
+        {
+          id: 1,
+          title: 'Crear proyecto',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Añadir componentes y servicios',
+          completed: false,
+        },
+      ];
 
-  tasks = signal<Task[]>([
-    {
-      id: 1,
-      title: 'Crear proyecto',
-      completed: true,
-    },
-    {
-      id: 2,
-      title: 'Añadir componentes y servicios',
-      completed: false,
-    },
-  ]);
+  tasks = signal<Task[]>(this.initialTasks);
+
+  private nextId = this.tasks().length + 1;
 
   totalTasks = computed(() => this.tasks().length);
 
   completedTasks = computed(() => this.tasks().filter((task) => task.completed).length);
 
   pendingTasks = computed(() => this.totalTasks() - this.completedTasks());
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem(this.storageKey, JSON.stringify(this.tasks()));
+    });
+  }
 
   addTask(title: string): void {
     const cleanTitle = title.trim();
